e876c56b6363cf48a335c10b330f1ba7617c5cca
search99 <ontomail@gmail.com>
2024-11-29 06:43:25 +0800

    fix

--- Vless_workers_pages/vless源码(请使用上面的_worker.js，本文件无法使用，仅供DIY).js
+++ Vless_workers_pages/vless源码(请使用上面的_worker.js，本文件无法使用，仅供DIY).js
@@ -1,5 +1,6 @@
 // <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
+// fix
 import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
